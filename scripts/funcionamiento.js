7ceea217ef188e042df37e92c949f9c400db15ea
inaDAGod <danialee14@gmail.com>
2024-03-03 18:29:32 -0400

    correciones

--- scripts/funcionamiento.js
+++ scripts/funcionamiento.js
@@ -1,14 +1,4 @@
-let grafo;
-let nodosDataSet;
-let aristasDataSet;
-let seleccionado;
-let modoEliminarArista = false;
-let modoEliminarNodo = false;
-let modoAgregarNodo = false; 
-let modoAgregarArista = false;
-let btnActivos = 0;
-let dobleClicEnNodoManejado = false;
-let modoCambiarColorNodo = false;
+
 let nodoSeleccionadoId = null;
 
 let buttonStates = {
@@ -30,7 +20,7 @@ function setActiveButton(buttonId) {
     }
     buttonStates[buttonId] = !buttonStates[buttonId];
 }
-function desactivarBotones() {
+function desactivarBotones2() {
     for (const key in buttonStates) {
         if (buttonStates[key]) {
             toggleButton(key);
@@ -39,70 +29,19 @@ function desactivarBotones() {
     }
 }
 
-// Función para inicializar el grafo
-function inicializarGrafo() {
-  const lienzo = document.getElementById('lienzo');
-  nodosDataSet = new vis.DataSet();
-  aristasDataSet = new vis.DataSet();
-  const data = { nodes: nodosDataSet, edges: aristasDataSet };
-  const opciones = {};
-  grafo = new vis.Network(lienzo, data, opciones);
-  grafo.on('doubleClick', dobleClicEnNodo);
-  grafo.on('doubleClick', dobleClicEnArista);
-}
-function desactivarBotones(){
-    grafo.off('click',eliminarArista);
-    modoEliminarArista = false;
-    grafo.off('click', eliminarNodo);
-    modoEliminarNodo = false;
-    grafo.off('click', clicEnNodo);
-    modoAgregarArista = false;
-    grafo.off('click',agregarNodo);  
-    modoAgregarNodo = false; 
-    grafo.off('click', cambiarColorNodo);
-    modoCambiarColorNodo = false;
-    btnActivos = 0;
-}
-function clicEnNodo(propiedades) {
-  const { nodes } = propiedades;
-  if (nodes.length > 0) {
-    if (seleccionado === undefined) {
-      seleccionado = nodes[0];
-    } else {
-      if (seleccionado !== nodes[0]) {
-        aristasDataSet.add({ from: seleccionado, to: nodes[0], arrows: 'to' });
-        seleccionado = undefined;
-      } else {
-        aristasDataSet.add({ from: seleccionado, to: seleccionado, arrows: 'to' });
-        seleccionado = undefined;
-      }
-    }
-  }
-}
 
 
-
+/*
 function dobleClicEnNodo(propiedades) {
     const { nodes } = propiedades;
     if (nodes.length > 0) {
         const nodeId = nodes[0];
-        // Muestra las herramientas para editar el nodo
         mostrarHerramientas(nodeId);
     }
 }
 
 
-function nombreNodo(propiedades) {
-    const { nodes } = propiedades;
-    if (nodes.length > 0) {
-        const nodeId = nodes[0];
-        const nuevoNombre = prompt('Ingrese el nuevo nombre para el nodo:', nodosDataSet.get(nodeId).label);
-        if (nuevoNombre !== null) {
-            nodosDataSet.update({ id: nodeId, label: nuevoNombre });
-        }
-        dobleClicEnNodoManejado = true;
-    }
-}
+ 
 
 
 
@@ -122,148 +61,13 @@ function dobleClicEnArista(propiedades) {
     }
 }
 
-function agregarNodoSeleccionado(){
-    toggleButton('nodeButton');
-    setActiveButton('nodeButton');
-    if(modoAgregarNodo){
-        btnActivos--;
-        modoAgregarNodo = false;
-        grafo.off('click',agregarNodo);   
-    }
-    else{
-        if(btnActivos > 0){
-            desactivarBotones();
-        }
-        btnActivos++;
-        modoAgregarNodo = true;
-        grafo.on('click',agregarNodo);  
-    }
-}
-function agregarAristaSeleccionada(){
-    toggleButton('edgeButton');
-    setActiveButton('edgeButton');
-    if(modoAgregarArista){
-        btnActivos--;
-        modoAgregarArista = false;
-        grafo.off('click', clicEnNodo);
-    }
-    else{
-        if(btnActivos > 0){
-            desactivarBotones();
-        }
-        btnActivos++;
-        modoAgregarArista = true;
-        grafo.on('click', clicEnNodo);
-    }
-    
-}
-
-function eliminarNodoSeleccionado() {
-    toggleButton('deleteNodeButton');
-    setActiveButton('deleteNodeButton');
-    if (modoEliminarNodo) {
-        btnActivos--;
-        modoEliminarNodo = false;
-        grafo.off('click', eliminarNodo);
-    } else {
-        if(btnActivos > 0){
-            desactivarBotones();
-        }
-        btnActivos++;
-        modoEliminarNodo = true;
-        grafo.on('click', eliminarNodo);
-    }
-}
-
-function eliminarAristaSeleccionada() {
-    toggleButton('deleteEdgeButton');
-    setActiveButton('deleteEdgeButton');
-    if (modoEliminarArista) {
-        btnActivos--;
-        modoEliminarArista = false;
-        grafo.off('click', eliminarArista);
-    } else {
-        if(btnActivos > 0){
-            desactivarBotones();
-        }
-        btnActivos++;
-        modoEliminarArista = true;
-        grafo.on('click', eliminarArista);
-    }
-}
-
-function agregarNodo(event){
-    nodosDataSet.add({ id: nodosDataSet.length + 1, label: 'Nodo ' + (nodosDataSet.length + 1), x: event.pointer.canvas.x, y: event.pointer.canvas.y });
-}
-
-  
+*/
 
 
 
 
 
-function eliminarNodo(event) {
-    const nodeId = event.nodes[0]; 
-    nodosDataSet.remove({ id: nodeId }); 
-}
 
-
-
-function eliminarArista(event) {
-    const edgeId = event.edges[0]; 
-    aristasDataSet.remove({ id: edgeId }); 
-}
-
-
-
-
-
-function generarMatriz() {
-  const nodos = nodosDataSet.get({ fields: ['id', 'label'] });
-  const matriz = [];
-  nodos.forEach(nodo => {
-    const fila = [];
-    nodos.forEach(otroNodo => {
-      const conexion = aristasDataSet.get({
-        filter: edge =>
-          (edge.from === nodo.id && edge.to === otroNodo.id)
-      });
-      if (conexion.length > 0) {
-        fila.push(parseInt(conexion[0].label || 1));
-      } else {
-        fila.push(0); 
-      }
-    });
-    matriz.push(fila);
-  });
-  mostrarMatriz(nodos, matriz);
-}
-
-function mostrarMatriz(nodos, matriz) {
-  const contenedorMatriz = document.getElementById('matriz');
-  let html = '<h2>Matriz de Adyacencia</h2>';
-  html += '<table>';
-  html += '<tr><th></th>';
-  nodos.forEach((nodo, index) => {
-    html += `<th>${nodo.label}</th>`;
-  });
-  html += '</tr>';
-  matriz.forEach((fila, index) => {
-    html += `<tr><th>${nodos[index].label}</th>`;
-    fila.forEach(valor => {
-      html += `<td>${valor}</td>`;
-    });
-    html += '</tr>';
-  });
-  html += '</table>';
-  contenedorMatriz.innerHTML = html;
-}
-
-function limpiar(){
-    inicializarGrafo();
-}
-
-//Pruebas
 function cambiarColorLienzo() {
   const color = document.getElementById('colorSelector').value;
   const lienzo = document.getElementById('lienzo');
@@ -315,10 +119,7 @@ function cambiarColorNodoSeleccionado() {
     }
 }
 
-//fin prueba
-document.addEventListener('DOMContentLoaded', () => {
-  inicializarGrafo();
-});
+
 
 // Función para exportar la información a un archivo JSON
 function exportarAJSON() {
--- scripts/scriptD.js
+++ scripts/scriptD.js
@@ -44,6 +44,7 @@ function desactivarBotones(){
     modoCambiarColorNodo = false;
     btnActivos = 0;
 }
+
 function clicEnNodo(propiedades) {
   const { nodes } = propiedades;
   if (nodes.length > 0) {
@@ -78,76 +79,26 @@ function clicEnNodo2(propiedades) {
     }
   }
 }
-function exportarAJSON() {
-  // Obtener la información de los nodos y aristas
-  const nodos = nodosDataSet.get({ returnType: "Object" });
-  const aristas = aristasDataSet.get({ returnType: "Object" });
 
-  // Crear un objeto que contenga la información a guardar
-  const informacion = {
-      nodos: nodos,
-      aristas: aristas
-  };
 
-  // Convertir la información en formato JSON
-  const informacionJSON = JSON.stringify(informacion, null, 2);
 
-  // Crear un objeto Blob con el contenido JSON
-  const blob = new Blob([informacionJSON], { type: 'application/json' });
-
-  // Crear una URL para el Blob
-  const url = URL.createObjectURL(blob);
-
-  // Crear un enlace para descargar el archivo JSON
-  const enlace = document.createElement('a');
-  enlace.href = url;
-  enlace.download = 'informacion.json';
-
-  // Agregar el enlace al cuerpo del documento y hacer clic en él para iniciar la descarga
-  document.body.appendChild(enlace);
-  enlace.click();
-
-  // Liberar los recursos del objeto URL
-  URL.revokeObjectURL(url);
-}
-
-let buttonStates = {
-  nodeButton: false,
-  edgeButton: false,
-  deleteEdgeButton: false,
-  deleteNodeButton: false
-};
-function toggleButton(buttonId) {
-  var button = document.getElementById(buttonId);
-  button.classList.toggle('active'); // Toggle the 'active' class
-}
-function setActiveButton(buttonId) {
-  for (const key in buttonStates) {
-      if (key !== buttonId && buttonStates[key]) {
-          toggleButton(key);
-          buttonStates[key] = false;
-      }
-  }
-  buttonStates[buttonId] = !buttonStates[buttonId];
-}
-function desactivarBotones() {
-  for (const key in buttonStates) {
-      if (buttonStates[key]) {
-          toggleButton(key);
-          buttonStates[key] = false;
-      }
-  }
-}
 
 
 function dobleClicEnNodo(propiedades) {
-  const { nodes } = propiedades;
-  if (nodes.length > 0) {
-      const nodeId = nodes[0];
-      // Muestra las herramientas para editar el nodo
-      mostrarHerramientas(nodeId);
-  }
+  desactivarBotones();
+    const { nodes } = propiedades;
+    if (nodes.length > 0) {
+        const nodeId = nodes[0];
+        const nuevoNombre = prompt('Ingrese el nuevo nombre para el nodo:', nodosDataSet.get(nodeId).label);
+        if (nuevoNombre !== null) {
+          
+            nodosDataSet.update({ id: nodeId, label: nuevoNombre });
+        }
+        dobleClicEnNodoManejado = true;
+    }
 }
+
+
 
 
 function dobleClicEnArista(propiedades) {
@@ -171,19 +122,19 @@ function dobleClicEnArista(propiedades) {
 
 function eliminarAristaSeleccionada() {
   toggleButton('deleteEdgeButton');
-  setActiveButton('deleteEdgeButton');
-  if (modoEliminarArista) {
-      btnActivos--;
-      modoEliminarArista = false;
-      grafo.off('click', eliminarArista);
-  } else {
-      if(btnActivos > 0){
-          desactivarBotones();
-      }
-      btnActivos++;
-      modoEliminarArista = true;
-      grafo.on('click', eliminarArista);
-  }
+    setActiveButton('deleteEdgeButton');
+    if (modoEliminarArista) {
+        btnActivos--;
+        modoEliminarArista = false;
+        grafo.off('click', eliminarArista);
+    } else {
+        if(btnActivos > 0){
+            desactivarBotones();
+        }
+        btnActivos++;
+        modoEliminarArista = true;
+        grafo.on('click', eliminarArista);
+    }
 }
 
 
@@ -194,6 +145,8 @@ function eliminarArista(event) {
 
 
 function agregarAristaSeleccionada(){
+    toggleButton('edgeButton');
+    setActiveButton('edgeButton');
     if(modoAgregarArista){
         btnActivos--;
         modoAgregarArista = false;
@@ -210,42 +163,40 @@ function agregarAristaSeleccionada(){
     
 }
 
-function agregarAristaSeleccionada(){
-  toggleButton('edgeButton');
-  setActiveButton('edgeButton');
-  if(modoAgregarArista){
+function agregarAristaSeleccionada2(){
+  if(modoAgregarArista2){
       btnActivos--;
-      modoAgregarArista = false;
-      grafo.off('click', clicEnNodo);
+      modoAgregarArista2 = false;
+      grafo.off('click', clicEnNodo2);
   }
   else{
       if(btnActivos > 0){
           desactivarBotones();
       }
       btnActivos++;
-      modoAgregarArista = true;
-      grafo.on('click', clicEnNodo);
+      modoAgregarArista2 = true;
+      grafo.on('click', clicEnNodo2);
   }
-
+  
 }
 
 
 function agregarNodoSeleccionado(){
-  toggleButton('nodeButton');
-  setActiveButton('nodeButton');
-  if(modoAgregarNodo){
-      btnActivos--;
-      modoAgregarNodo = false;
-      grafo.off('click',agregarNodo);   
-  }
-  else{
-      if(btnActivos > 0){
-          desactivarBotones();
-      }
-      btnActivos++;
-      modoAgregarNodo = true;
-      grafo.on('click',agregarNodo);  
-  }
+    toggleButton('nodeButton');
+    setActiveButton('nodeButton');
+    if(modoAgregarNodo){
+        btnActivos--;
+        modoAgregarNodo = false;
+        grafo.off('click',agregarNodo);   
+    }
+    else{
+        if(btnActivos > 0){
+            desactivarBotones();
+        }
+        btnActivos++;
+        modoAgregarNodo = true;
+        grafo.on('click',agregarNodo);  
+    }
 }
 
 function agregarNodo(event){
@@ -258,26 +209,32 @@ function agregarNodo(event){
 
 
 function eliminarNodoSeleccionado() {
-  toggleButton('deleteNodeButton');
-  setActiveButton('deleteNodeButton');
-  if (modoEliminarNodo) {
-      btnActivos--;
-      modoEliminarNodo = false;
-      grafo.off('click', eliminarNodo);
-  } else {
-      if(btnActivos > 0){
-          desactivarBotones();
-      }
-      btnActivos++;
-      modoEliminarNodo = true;
-      grafo.on('click', eliminarNodo);
-  }
+    toggleButton('deleteNodeButton');
+    setActiveButton('deleteNodeButton');
+    if (modoEliminarNodo) {
+        btnActivos--;
+        modoEliminarNodo = false;
+        grafo.off('click', eliminarNodo);
+    } else {
+        if(btnActivos > 0){
+            desactivarBotones();
+        }
+        btnActivos++;
+        modoEliminarNodo = true;
+        grafo.on('click', eliminarNodo);
+    }
 }
 
 
 function eliminarNodo(event) {
-  const nodeId = event.nodes[0]; 
-  nodosDataSet.remove({ id: nodeId }); 
+    const nodeId = event.nodes[0]; 
+    const aristasAEliminar = aristasDataSet.get({ filter: item => item.from === nodeId || item.to === nodeId });
+    nodosDataSet.remove({ id: nodeId });
+    aristasAEliminar.forEach(arista => {
+        aristasDataSet.remove({ id: arista.id });
+    });
+
+    
 }
 
 
@@ -359,52 +316,8 @@ function limpiar(){
     inicializarGrafo();
 }
 
-//Pruebas
-function cambiarColorLienzo() {
-  const color = document.getElementById('colorSelector').value;
-  const lienzo = document.getElementById('lienzo');
-  lienzo.style.backgroundColor = color;
-}
-//Prueba color de nodo
-function cambiarColorNodoSeleccionado() {
-  const colorSelectorNode = document.getElementById('colorSelectorNode');
-  if (modoCambiarColorNodo) {
-      // Desactivar el modo de cambiar color del nodo
-      btnActivos--;
-      modoCambiarColorNodo = false;
-      grafo.off('click'); // Desactivar todos los eventos de clic en el grafo
-      colorSelectorNode.style.display = 'none'; // Ocultar el selector de color
-  } else {
-      if (btnActivos > 0) {
-          // Si hay otros botones activos, desactivarlos
-          desactivarBotones();
-      }
-      // Activar el modo de cambiar color del nodo
-      btnActivos++;
-      modoCambiarColorNodo = true;
-      grafo.on('click', function (event) {
-          const nodeId = event.nodes[0];
-          if (nodeId !== undefined) {
-              // Si se hizo clic en un nodo, cambiar su color
-              colorSelectorNode.style.display = 'inline-block'; // Mostrar el selector de color
-              colorSelectorNode.onchange = function () {
-                  const color = colorSelectorNode.value;
-                  nodosDataSet.update({ id: nodeId, color: { background: color, border: color } });
-                  colorSelectorNode.style.display = 'none'; // Ocultar el selector de color
-              };
-          }
-      });
-  }
-}
 
-function cambiarColorNodo(event) {
-  const nodeId = event.nodes[0];
-  const color = document.getElementById('colorSelectorNode').value;
-  // Cambiar el color del nodo
-  nodosDataSet.update({ id: nodeId, color: { background: color, border: color } });
-}
 
-//fin prueba
 document.addEventListener('DOMContentLoaded', () => {
   inicializarGrafo();
 });
