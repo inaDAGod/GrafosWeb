cc60ba48cb23ded9d9985ced2c03329f93efc500
Jhuly1215 <jhuliannnavitoria@gmail.com>
2024-03-03 17:48:01 -0400

    func doble click nodo

--- scripts/scriptD.js
+++ scripts/scriptD.js
@@ -78,21 +78,76 @@ function clicEnNodo2(propiedades) {
     }
   }
 }
+function exportarAJSON() {
+  // Obtener la información de los nodos y aristas
+  const nodos = nodosDataSet.get({ returnType: "Object" });
+  const aristas = aristasDataSet.get({ returnType: "Object" });
 
+  // Crear un objeto que contenga la información a guardar
+  const informacion = {
+      nodos: nodos,
+      aristas: aristas
+  };
+
+  // Convertir la información en formato JSON
+  const informacionJSON = JSON.stringify(informacion, null, 2);
+
+  // Crear un objeto Blob con el contenido JSON
+  const blob = new Blob([informacionJSON], { type: 'application/json' });
+
+  // Crear una URL para el Blob
+  const url = URL.createObjectURL(blob);
+
+  // Crear un enlace para descargar el archivo JSON
+  const enlace = document.createElement('a');
+  enlace.href = url;
+  enlace.download = 'informacion.json';
+
+  // Agregar el enlace al cuerpo del documento y hacer clic en él para iniciar la descarga
+  document.body.appendChild(enlace);
+  enlace.click();
+
+  // Liberar los recursos del objeto URL
+  URL.revokeObjectURL(url);
+}
+
+let buttonStates = {
+  nodeButton: false,
+  edgeButton: false,
+  deleteEdgeButton: false,
+  deleteNodeButton: false
+};
+function toggleButton(buttonId) {
+  var button = document.getElementById(buttonId);
+  button.classList.toggle('active'); // Toggle the 'active' class
+}
+function setActiveButton(buttonId) {
+  for (const key in buttonStates) {
+      if (key !== buttonId && buttonStates[key]) {
+          toggleButton(key);
+          buttonStates[key] = false;
+      }
+  }
+  buttonStates[buttonId] = !buttonStates[buttonId];
+}
+function desactivarBotones() {
+  for (const key in buttonStates) {
+      if (buttonStates[key]) {
+          toggleButton(key);
+          buttonStates[key] = false;
+      }
+  }
+}
 
 
 
 
 function dobleClicEnNodo(propiedades) {
-  desactivarBotones();
     const { nodes } = propiedades;
     if (nodes.length > 0) {
         const nodeId = nodes[0];
-        const nuevoNombre = prompt('Ingrese el nuevo nombre para el nodo:', nodosDataSet.get(nodeId).label);
-        if (nuevoNombre !== null) {
-            nodosDataSet.update({ id: nodeId, label: nuevoNombre });
-        }
-        dobleClicEnNodoManejado = true;
+        // Muestra las herramientas para editar el nodo
+        mostrarHerramientas(nodeId);
     }
 }
 
