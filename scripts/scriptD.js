b314b9275eaf944e8c9dfdf3e3761818012546d4
inaDAGod <danialee14@gmail.com>
2024-03-10 20:46:24 -0400

    colores matriz y sumas

--- scripts/scriptD.js
+++ scripts/scriptD.js
@@ -283,41 +283,60 @@ function mostrarMatriz(nodos, matriz) {
   if(nodos.length > 0){
   let html = '<h2>Matriz de Adyacencia</h2>';
   let entradas=[];
+  let entradas2=[];
   html += '<table>';
-  html += '<tr><th></th>';
+  html += '<tr><th style = " background: #473179;"></th>';
   let columna = 0;
   nodos.forEach((nodo, index) => {
-    html += `<th>${nodo.label}</th>`;
+    html += `<th style = " background: #9E7DD4;">${nodo.label}</th>`;
     entradas[columna] = 0;
+    entradas2[columna] = 0;
     columna++;
   });
-  html += '<th>Grado de salida</th>';
+  html += '<th style = " background: #FBAD41;">Grado de salida</th><th style = " background: #FD918F;">Suma de salida</th>';
   html += '</tr>';
  
   matriz.forEach((fila, index) => {
-    html += `<tr><th>${nodos[index].label}</th>`;
+    html += `<tr><th style = " background: #9E7DD4;">${nodos[index].label}</th>`;
     let salidas = 0;
+    let salida = 0;
     columna = 0;
     fila.forEach(valor => {
-      html += `<td>${valor}</td>`;
+      html += `<td style = " background: #BCB9D8;">${valor}</td>`;
       if(valor != 0){
         salidas++;
+        salida+=valor;
         entradas[columna]++;
+        entradas2[columna]+=valor;
       }
       columna++;
     });
     
-    html += `<td>${salidas}</td>`;
+    html += `<th style = " background: #fdd092;">${salidas}</th>`;
+    html += `<th style = " background: #f8b3b2;">${salida}</th>`;
   });
-  html += '</tr><th>Grado de Entrada</th>';
+  
+  html += '</tr><th style = " background: #FBAD41;">Grado de Entrada</th>';
   columna = 0;
   entradas.forEach(element => {
-    html += `<td>${element}</td>`;
+    html += `<th style = " background: #fdd092;">${element}</th>`;
     if(element > 0){
       columna += element;
     }
   });
-  html += `<th>${columna}</th>`;
+  html += `<th style = " background: #f8be6d;">${columna}</th>`;
+  html += '<th style = " background: #f8b3b2;"></th>';
+  html += '<tr>'
+  html += '</tr><th style = " background: #FD918F;">Suma de Entrada</th>';
+  columna = 0;
+  entradas2.forEach(element => {
+    html += `<th style = " background: #f8b3b2;">${element}</th>`;
+    if(element > 0){
+      columna += element;
+    }
+  });
+  html += '<th style = " background: #f8b3b2;"></th>';
+  html += `<th style = " background: #fd9a98;">${columna}</th>`;
   html += '<tr>'
   html += '</table>';
   contenedorMatriz.innerHTML = html;
@@ -409,6 +428,8 @@ function dobleClicEnArista(propiedades) {
 
 
 function cambiarColorLienzo() {
+  desactivarBotones();
+  desactivarBotones2();
   const color = document.getElementById('colorSelector').value;
   const lienzo = document.getElementById('lienzo');
   lienzo.style.backgroundColor = color;
